8016cbb75fabaf46d3aeae2a95188a10bd615542
skinwalker <skinrr932@gmail.com>
2025-08-08 14:59:37 +0300

    Update stack.h

--- src/stack.h
+++ src/stack.h
@@ -25,13 +25,10 @@ int IsValid_arg(int parsed, int args);
 
 //colors
 #define RESET        "\033[0m"
-#define BLACK        "\033[30m"
 #define RED          "\033[31m"
 #define GREEN        "\033[32m"
 #define YELLOW       "\033[33m"
-#define BLUE         "\033[34m"
 #define LIGHT_GREEN  "\033[35m"
 #define CYAN         "\033[36m"
-#define WHITE        "\033[37m"
 
 #endif
